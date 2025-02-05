a7138a78476d7c204f062115ee9b3bc906a59477
vinod kumar <vinnuvinodkumar8080@gmail.com>
2025-02-05 13:15:25 +0530

    app config and env added

---
+++ projects/host/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  urls: {
+    mfe: 'https://golden-stardust-dd6a7b.netlify.app/',
+    akitaCounter: 'https://endearing-hamster-d97593.netlify.app/',
+    flights: 'https://stunning-bavarois-9e6e52.netlify.app/',
+    hospital: 'https://gleeful-buttercream-4e661c.netlify.app/',
+  },
+};
---
+++ projects/host/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  urls: {
+    dashboard: 'http://localhost:4300',
+    cart: 'http://localhost:4400',
+  },
+};
