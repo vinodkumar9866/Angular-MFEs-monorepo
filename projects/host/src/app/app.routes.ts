4077b000e654461ed875cbf43812545261c31cf5
vinod kumar <vinnuvinodkumar8080@gmail.com>
2025-02-05 13:40:45 +0530

    urls updated

--- projects/host/src/app/app.routes.ts
+++ projects/host/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { loadRemoteModule } from '@angular-architects/module-federation';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 export const routes: Routes = [
   {
